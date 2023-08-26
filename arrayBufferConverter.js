1969a491c9c1d9eb0453f0ca694e2bcee2f6b507
Давыдов Александр <madyson123@yandex.ru>
2023-08-26 20:35:29 +0300

    Added task

---
+++ arrayBufferConverter.js
@@ -0,0 +1,15 @@
+export default class ArrayBufferConverter {
+    #buffer
+    constructor(buffer) {
+        this.#buffer = buffer;
+    }
+
+    toString() {
+        let str = '';
+        const bufferUint = new Uint16Array(this.#buffer);
+        bufferUint.forEach((e) => {
+            str += String.fromCharCode(e);
+        });
+        return str;
+    }
+};
